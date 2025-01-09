c59739f4a03a7558fba37c08b8de202a2d1c87d8
ProjectsRO-hit <official.rohitsingh22@gmail.com>
2025-01-09 15:05:11 +0530

    adding files

---
+++ main.py
@@ -0,0 +1,13 @@
+# from turtle import Turtle, Screen
+import pandas as pd
+from trivia_brain import TriviaBrain
+
+#setting object for screen
+trivia = TriviaBrain()
+
+#setting welcome screen
+trivia.welcome()
+
+#setting up pandas
+
+   
---
+++ trivia_brain.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+class TriviaBrain:
+    def __init__(self):
+        self.screen = Screen()
+        self.screen.setup(width=600, height=500)
+        self.screen.title("Trivia Game")
+        self.image = ["./trivia/1.gif", "./trivia/2.gif", "./trivia/3.gif", "./trivia/4.gif", "./trivia/5.gif"]
+        self.turtle = Turtle()
+        self.turtle.hideturtle()
+    
+    def welcome(self):
+        self.screen.addshape(self.image[0])
+        self.turtle.shape(self.image[0])
+        self.turtle.showturtle()  # Call showturtle() with parentheses to make sure it works
+        self.continue_game = self.screen.textinput("Welcome to Trivia Game", "Do you want to play the game? Type 'yes' to continue else 'no' to exit")
+        if self.continue_game.lower() == "yes":
+            self.turtle.hideturtle()
+            self.instructions()
+        else:
+            self.screen.bye()
+            exit()
+
+    def instructions(self):
+        self.screen.addshape(self.image[1])
+        self.turtle.shape(self.image[1])
+        self.turtle.showturtle()  # Ensure showturtle() is called properly
+        self.continue_game = self.screen.textinput("Instructions", "Did you read the instructions? Type 'yes' to continue else 'no' to exit")
+        if self.continue_game.lower() == "yes":
+            pass
+        else:
+            self.screen.bye()
+            exit()
+
